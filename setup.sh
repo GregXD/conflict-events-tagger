18f020c62473c2d67812348c23009e0317ad873c
gamaly <gamaly@gmail.com>
2024-08-05 07:28:30 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 mkdir -p ~/.streamlit/
 
+
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS = false\n\
+enableCORS=false\n\
 port = $PORT\n\
+\n\
+[theme]\n\
+base='dark'\n\
+primaryColor='#7a9ae5'\n\
 " > ~/.streamlit/config.toml
+
